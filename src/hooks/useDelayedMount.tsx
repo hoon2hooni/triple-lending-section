b09018bf8c53458abdbe6599a8d04d2b0c578aef
hoon2hooni <sanghoon.engineer@gmail.com>
2022-07-06 10:40:55 +0900

    feat: make useDelayedMount custom hooks to mount component sequentially

---
+++ src/hooks/useDelayedMount.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+
+const useDelayedMount = (ms: number) => {
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => {
+    setTimeout(() => setIsMounted(true), ms)
+  }, [ms])
+  return isMounted
+}
+
+export default useDelayedMount
