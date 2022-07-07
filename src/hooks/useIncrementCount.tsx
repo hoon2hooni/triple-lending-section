6d1487407972a7381668ca02154a7f9eebe8f632
hoon2hooni <sanghoon.engineer@gmail.com>
2022-07-07 12:16:44 +0900

    fix: stop calling requestanimation function when it reached target

--- src/hooks/useIncrementCount.tsx
+++ src/hooks/useIncrementCount.tsx
@@ -1,10 +1,22 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
+import {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  MutableRefObject,
+} from 'react'
 
 import { easeOutExpo } from '@utils/easeOut'
 
 const increaseCountUntilMax =
-  (currTime: number, maxNum: number) => (state: number) => {
+  (
+    currTime: number,
+    maxNum: number,
+    ref: MutableRefObject<number | undefined>,
+  ) =>
+  (state: number) => {
     if (state >= maxNum) {
+      ref.current = undefined
       return maxNum
     }
     return Math.floor(easeOutExpo(currTime, 0, maxNum, 2))
@@ -12,26 +24,29 @@ const increaseCountUntilMax =
 
 const useIncrementCount = (max: number) => {
   const [count, setCount] = useState(0)
-  const requestId = useRef<number>()
   const prevTimeRef = useRef<number>()
+  const requestRef = useRef<number>()
   const currentTimeRef = useRef<number>(0)
 
   const animate = useCallback(
     (time: number) => {
+      if (!requestRef.current) {
+        return
+      }
+      setCount(increaseCountUntilMax(currentTimeRef.current, max, requestRef))
       if (prevTimeRef.current) {
         const deltaTime = (time - prevTimeRef.current) / 1000
-        setCount(increaseCountUntilMax(currentTimeRef.current, max))
         currentTimeRef.current += deltaTime
       }
       prevTimeRef.current = time
-      requestId.current = requestAnimationFrame(animate)
+      requestRef.current = requestAnimationFrame(animate)
     },
     [max],
   )
 
   useEffect(() => {
-    requestId.current = requestAnimationFrame(animate)
-    return () => cancelAnimationFrame(requestId.current as number)
+    requestRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(requestRef.current as number)
   }, [max, animate])
   return count
 }
