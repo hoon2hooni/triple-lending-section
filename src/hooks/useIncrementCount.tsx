4f2c8668ba75470d1df3e4f611a044d9bb983dd9
hoon2hooni <sanghoon.engineer@gmail.com>
2022-07-07 16:26:01 +0900

    refac: change variable name and change effect function from useEffect to useLayoutEffect

--- src/hooks/useIncrementCount.tsx
+++ src/hooks/useIncrementCount.tsx
@@ -1,53 +1,51 @@
 import {
   useState,
-  useEffect,
   useRef,
   useCallback,
   MutableRefObject,
+  useLayoutEffect,
 } from 'react'
 
 import { easeOutExpo } from '@utils/easeOut'
 
 const increaseCountUntilMax =
-  (
-    currTime: number,
-    maxNum: number,
-    ref: MutableRefObject<number | undefined>,
-  ) =>
+  (elapsedTime: number, target: number, ref: MutableRefObject<boolean>) =>
   (state: number) => {
-    if (state >= maxNum) {
-      ref.current = undefined
-      return maxNum
+    if (state >= target) {
+      ref.current = false
+      return target
     }
-    return Math.floor(easeOutExpo(currTime, 0, maxNum, 2))
+    return Math.floor(easeOutExpo(elapsedTime, 0, target, 2))
   }
 
-const useIncrementCount = (max: number) => {
+const useIncrementCount = (target: number) => {
   const [count, setCount] = useState(0)
   const prevTimeRef = useRef<number>()
-  const requestRef = useRef<number>()
-  const currentTimeRef = useRef<number>(0)
-
+  const rafRef = useRef<number>()
+  const elapsedTimeRef = useRef<number>(0)
+  const flagRef = useRef<boolean>(true)
   const animate = useCallback(
-    (time: number) => {
-      if (!requestRef.current) {
-        return
-      }
-      setCount(increaseCountUntilMax(currentTimeRef.current, max, requestRef))
+    (currentTime: number) => {
+      setCount(increaseCountUntilMax(elapsedTimeRef.current, target, flagRef))
       if (prevTimeRef.current) {
-        const deltaTime = (time - prevTimeRef.current) / 1000
-        currentTimeRef.current += deltaTime
+        const deltaTime = (currentTime - prevTimeRef.current) / 1000
+        elapsedTimeRef.current += deltaTime
       }
-      prevTimeRef.current = time
-      requestRef.current = requestAnimationFrame(animate)
+      prevTimeRef.current = currentTime
+      rafRef.current = requestAnimationFrame(animate)
+      if (!flagRef.current) {
+        cancelAnimationFrame(rafRef.current)
+      }
     },
-    [max],
+    [target],
   )
 
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate)
-    return () => cancelAnimationFrame(requestRef.current as number)
-  }, [max, animate])
+  useLayoutEffect(() => {
+    rafRef.current = requestAnimationFrame(animate)
+    return () => {
+      rafRef.current && cancelAnimationFrame(rafRef.current)
+    }
+  }, [target, animate])
   return count
 }
 
